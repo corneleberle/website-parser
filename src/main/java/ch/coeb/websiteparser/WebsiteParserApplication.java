eeca4053235edc04d34ddc5337db842699741113
Cornel Eberle <cornel.eberle@gmail.com>
2015-04-04 12:15:16 +0200

    init eclipse project

---
+++ src/main/java/ch/coeb/websiteparser/WebsiteParserApplication.java
@@ -0,0 +1,12 @@
+package ch.coeb.websiteparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteParserApplication.class, args);
+    }
+}
